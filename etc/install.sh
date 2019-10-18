27de5f463e60a7359cc500b5a88edcb552ee9d1d
rn1k <maki_r12@yahoo.co.jp>
2019-10-18 15:29:59 +0900

    Update install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -70,6 +70,7 @@ elif [ `uname` = "Darwin" ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew install zsh
     brew install zsh-completion
+    brew install yarn
     brew install tmux
     brew install zplug
     brew install neovim
